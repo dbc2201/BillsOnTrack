7b09f15d8fead5f505a14f301d28a41c524901eb
dbc2201 <divyanshb22@gmail.com>
2022-02-18 21:29:45 +0530

    added library for log4j

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module io.dbc.github.billsontrack {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.apache.logging.log4j;
 
 
     opens io.dbc.github.billsontrack to javafx.fxml;
